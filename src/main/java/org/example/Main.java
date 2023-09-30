f67e067bdb4fabd70c18ba5632e61e4541eaf1ff
Willian <correo.willianrueda@gmail.com>
2023-09-30 07:49:07 -0500

    Funcionalidad menú de opciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +8,70 @@ public class Main {
         String category;
         String label;
         int price;
+
+        runMenu();
+
     }
+
+    public static void runMenu() {
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+            handleUserChoice(choice);
+        } while (choice != 8);
+    }
+
+    public static void handleUserChoice(int choice) {
+        switch (choice) {
+            case 1 -> addProduct();
+            case 2 -> removeProduct();
+            case 3 -> updateProduct();
+            case 4 -> System.out.println("Saliendo...");
+            default -> System.out.println("Opción invalida. Por favor intenta de nuevo.");
+        }
+
+    }
+
+    public static void addProduct(){
+        System.out.println("Producto agregado");
+    }
+
+    public static void removeProduct(){
+        System.out.println("Producto eliminado");
+    }
+
+    public static void updateProduct(){
+        System.out.println("Producto actualizado");
+    }
+
+    public static void displayMenu() {
+
+        System.out.println(
+                """
+                        |o|                         /////////////\\\\\\
+                        |o|                        (((((((((((((   \\\\\\
+                        |o|                        ))) ~~      ~~   (((
+                        |o|                        ((( (*)     (*)  )))
+                        |o|                        )))     <        (((\s
+                        |o|                        ((( '\\______/`   )))\s
+                        |o|                        )))\\___________/(((\s
+                        |o|                        (((   _)  (_    )))\s\s
+                        |o|                              /\\__/\\""");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio    |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar producto                   |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Salir                                  |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 5)  ");
+    }
+
+
+
 }
